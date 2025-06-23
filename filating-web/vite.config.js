aa22831815128381f14ae10d5d0f197a734b0a06
enowjohnenowbi <johnwashinton88@gmail.com>
2025-06-23 22:03:56 +0100

    upto date

--- filating-web/vite.config.js
+++ filating-web/vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/filating-web/' : '/',
+  server: {
+    port: 3000,
+    open: true
+  }
 })
